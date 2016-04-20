af5bfbd21a1fdef4a68d74725406eba3984a59eb
Oscar <contact@oscarhjelm.com>
2016-04-20 21:21:51 +0200

    added dev env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead','mkweb','webdev'),
+	'local' => array('homestead','mkweb','webdev','Iwasawa'),
         'production' => array('fluffy'),
 
 ));
