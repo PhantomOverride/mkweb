21da7998d629b93725399a15f1cda15a4e636174
Olof Haglund <olof@olofhaglund.name>
2017-01-03 23:27:42 +0100

    Get all files in directory

--- app/controllers/GalleryController.php
+++ app/controllers/GalleryController.php
@@ -3,13 +3,22 @@ class GalleryController extends BaseController{
     const PATH = "./img/gallery";
 
     public function index(){
-        $folders = $this->get_folders();
-        return View::make('gallery.index',['folders' => $folders])->with('nav', Page::navbar());
+        $directory = $this->get_folders();
+        return View::make('gallery.index',['folders' => $directory])->with('nav', Page::navbar());
+    }
+    public function show($directory){
+        $files = $this->get_files($directory);
+        return View::make('gallery.show',['directory' => $directory,'files' => $files])->with('nav', Page::navbar());
     }
 
     private function get_folders(){
         $directories = scandir(self::PATH);
         $directories = array_diff($directories, array('..', '.'));
         return $directories;
+    }
+    private function get_files($subdirectory){
+        $files = scandir(self::PATH . '/' . $subdirectory);
+        $files = array_diff($files, array('..', '.', 'thumbnail.jpg'));
+        return $files;
     }
 }
