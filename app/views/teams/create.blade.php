c852022a7b25471606d62acaeb8b7fe768d136a5
Olof Haglund <olof@olofhaglund.name>
2017-03-23 18:35:16 +0100

    La till info om deltagare

--- app/views/teams/create.blade.php
+++ app/views/teams/create.blade.php
@@ -23,41 +23,44 @@
     Kom ihåg att lägga till ditt kontonamn under ID så att CREW kan hitta dig i spelet! Är ni med i flera turneringar så markera
     vilket namn som är för vad.
 </p>
+<p>
+    <b>NOTE!</b> 3/5 deltagare måste vara närvarande på laget!
+</p>
 
     {{Form::open(['route' => 'teams.store'])}}
-    
+
     <div>
         {{Form::label('name', 'Lagnamn: ')}}
         {{Form::text('name')}}
         {{$errors->first('name', '<span class=error>:message</span>')}}
     </div>
-    
+
     <div>
         {{Form::label('motto', 'Lagmotto: ')}}
         {{Form::text('motto')}}
         {{$errors->first('motto', '<span class=error>:message</span>')}}
     </div>
-    
+
     <div>
         {{Form::label('leader', 'Lagledare: ')}}
         {{Form::text('leader',Auth::user()->nickname,array('readonly'))}}
         {{$errors->first('leader', '<span class=error>:message</span>')}}
     </div>
-    
+
     <div>
         {{Form::label('leadertags', 'Ledarens IDs: ')}}
         {{Form::text('leadertags')}}
         {{$errors->first('leadertags', '<span class=error>:message</span>')}}
     </div>
-    
+
     <div>
         {{Form::label('imageurl', 'Länk till logo: ')}}
         {{Form::text('imageurl')}}
         {{$errors->first('imageurl', '<span class=error>:message</span>')}}
     </div>
-    
+
     <div>{{Form::submit('Registrera laget!')}}</div>
-    
+
     {{Form::close()}}
 
 @stop
